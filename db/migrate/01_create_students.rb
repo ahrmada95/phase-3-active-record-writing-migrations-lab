e880a5abba651ecf15f5d3004726078eea9a7f47
Ahnaf <ahrmada95@gmail.com>
2022-08-17 17:58:23 -0400

    working with ActiveRecord

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    #create table
+    create_table :students do |t|
+    #create columns
+      t.string :name
+    end
   end
 end
